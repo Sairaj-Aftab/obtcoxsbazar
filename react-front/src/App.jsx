5adc9c1a723ff47f537a9689aa0fa779a55fe189
Sairaj-Aftab <sairajaftab6@gmail.com>
2024-08-05 03:13:19 +0600

    update code

--- react-front/src/App.jsx
+++ react-front/src/App.jsx
@@ -1,4 +1,5 @@
 import { RouterProvider } from "react-router-dom";
+import { Helmet } from "react-helmet";
 import router from "./routes/router";
 import { useDispatch } from "react-redux";
 import "./App.css";
@@ -60,6 +61,47 @@ function App() {
   }, [dispatch]);
   return (
     <>
+      <Helmet>
+        <title>
+          Online Bus Terminal (OBT) - Smart Bus Travel in Cox&apos;s Bazar
+        </title>
+        <meta
+          name="description"
+          content="Welcome to Online Bus Terminal (OBT) in Cox's Bazar. Discover the convenience of smart bus travel with our digital platform, offering easy booking, real-time updates, fare comparisons, and more. Enjoy a seamless travel experience with features like seat selection, schedule information, and 24/7 customer service. Explore our platform to find all bus services, destinations, and special offers, and connect with local and tourism industries for a safer, efficient, and enjoyable journey."
+        />
+        {/* Meta tags for Facebook */}
+        <meta
+          property="og:title"
+          content="Online Bus Terminal (OBT) - Smart Bus Travel in Cox's Bazar"
+        />
+        <meta
+          property="og:description"
+          content="Welcome to Online Bus Terminal (OBT) in Cox's Bazar. Discover the convenience of smart bus travel with our digital platform, offering easy booking, real-time updates, fare comparisons, and more. Enjoy a seamless travel experience with features like seat selection, schedule information, and 24/7 customer service. Explore our platform to find all bus services, destinations, and special offers."
+        />
+        <meta
+          property="og:image"
+          content="https://obtcoxsbazar.com/assets/red_yellow-BeM2oZga.png"
+        />
+        <meta property="og:url" content="https://www.obtcoxsbazar.com" />
+        <meta property="og:type" content="website"></meta>
+        {/* Meta tags for twitter */}
+        <meta
+          name="twitter:card"
+          content="https://obtcoxsbazar.com/assets/red_yellow-BeM2oZga.png"
+        />
+        <meta
+          name="twitter:title"
+          content="Online Bus Terminal (OBT) - Smart Bus Travel in Cox's Bazar"
+        />
+        <meta
+          name="twitter:description"
+          content="Welcome to Online Bus Terminal (OBT) in Cox's Bazar. Discover the convenience of smart bus travel with our digital platform, offering easy booking, real-time updates, fare comparisons, and more. Enjoy a seamless travel experience with features like seat selection, schedule information, and 24/7 customer service."
+        />
+        <meta
+          name="twitter:image"
+          content="https://obtcoxsbazar.com/assets/red_yellow-BeM2oZga.png"
+        />
+      </Helmet>
       <RouterProvider router={router} />
     </>
   );
