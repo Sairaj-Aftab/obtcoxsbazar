d41f40fbf5c455570bfe1af15b7bd6f56c88b8a8
Sairaj Aftab <sairajaftab6@gmail.com>
2025-02-28 02:10:50 +0600

    update a page

--- react-front/src/components/LostAndFoundHeader.jsx
+++ react-front/src/components/LostAndFoundHeader.jsx
@@ -1,14 +1,14 @@
 import { Link, Outlet, useLocation } from "react-router-dom";
-import NoticeFromAdmin from "./NoticeFromAdmin";
+// import NoticeFromAdmin from "./NoticeFromAdmin";
 
 const LostAndFoundHeader = () => {
   const { pathname } = useLocation();
 
   return (
     <div>
-      <div className="bg-white text-base font-semibold pt-1">
+      {/* <div className="bg-white text-base font-semibold pt-1">
         <NoticeFromAdmin status="Tourist-Bus-Permission" />
-      </div>
+      </div> */}
       <div className="bg-white rounded-t-md flex gap-3 w-fit mx-auto mt-1 sm:mt-3 p-2">
         <Link
           to="/lost-found"
